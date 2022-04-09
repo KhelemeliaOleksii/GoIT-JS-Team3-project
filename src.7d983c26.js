d00376717094eaa702b8a87207ac1024937e2a2b
KhelemeliaOleksii <89631480+KhelemeliaOleksii@users.noreply.github.com>
2022-04-09 00:45:59 +0000

    Deploying to gh-pages from @ KhelemeliaOleksii/GoIT-JS-Team3-project@aafd51b71c278aaafd1adf287abbd0be502725cb 🚀

--- src.7d983c26.js
+++ src.7d983c26.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/GoIT-JS-Team3-project/src.180c7f7f.js.map
+//# sourceMappingURL=/GoIT-JS-Team3-project/src.7d983c26.js.map
