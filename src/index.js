06bbb62919415588c1d9fa795538343cdaa0eaf8
Ivan <89925433+serene-ease@users.noreply.github.com>
2022-04-12 12:51:45 +0300

    made buttons and puggination function (but in not rendering becaus need Render page function)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './sass/main.scss';
-import './js/modal';
+// import './js/modal';
 import './js/pagButtons';
-import './js/pagination';
-import { arrForLocalStorage, LOCALSTORAGE_KEY_FOR_WATCHED, LOCALSTORAGE_KEY_FOR_QUEUE, addToLocalStorіageWhenStart } from "./js/localStorage";
+// import './js/pagination';
+// import { arrForLocalStorage, LOCALSTORAGE_KEY_FOR_WATCHED, LOCALSTORAGE_KEY_FOR_QUEUE, addToLocalStorіageWhenStart } from "./js/localStorage";
 
-addToLocalStorrageWhenStart();
+// addToLocalStorrageWhenStart();
