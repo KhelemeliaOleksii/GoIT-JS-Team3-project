92655c52fa14b9497001cd58bd65bdc928515f0f
Nadia Lut <3197344@gmail.com>
2022-04-14 19:56:16 +0300

    current is done

--- src/js/header-home.js
+++ src/js/header-home.js
@@ -3,17 +3,29 @@
 const headerBox = document.querySelector('.header-js');
 const preview = document.querySelector('.preview');
 const containerEl = document.querySelector('.container');
-const visuallyHiddenHeaderBox = document.querySelector('.visually-hidden');
+const visuallyHiddenHeaderBox = document.querySelector('.nav-div');
 const visuallyHiddenHeaderBoxLib = document.querySelector('.library-header-img');
 const headerLibBtn = document.querySelector('.header__lib_link');
 const headerHomeBtn = document.querySelector('.header__home_link');
+const btnQueue = document.querySelector('.queue-btn-js');
+const btnWatched = document.querySelector('.watched-btn-js');
 
+// вешаем слушателя на кнопки QUEUE та WATCHED
+btnQueue.addEventListener('click', ()=>{ alert ('click on btnQueue')});
+btnWatched.addEventListener('click', () => { alert('click on btnWatched') });
 // вешаем слушателя на кнопку library 
 headerLibBtn.addEventListener('click', renderHeaderLib);
+// headerLibBtn.addEventListener('click', addCurrentStile);
 // вешаем слушателя на кнопку home 
 headerHomeBtn.addEventListener('click', renderHeaderHome);
 
-
+// подчеркивание кнопки
+// function addCurrentStile() {
+    
+//     headerLibBtn.classList.add('current');
+//     // alert('click on headerLibBtn');
+//     headerHomeBtn.classList.remove('current');
+// }
 
 // удаляет картинку-заставку через 2 сек
 function previewEl() {
@@ -41,6 +53,7 @@ function renderHeaderLib() {
     clearHeaderBox();
     // alert('перейти в library?');
     headerBox.classList.add('visually-hidden');
+    headerLibBtn.classList.add('current');
     visuallyHiddenHeaderBoxLib.classList.remove('visually-hidden');
 }
 
