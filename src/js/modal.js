c74af633d44f964962c20d1202077099556aa183
Denys <kanedius@gmail.com>
2022-04-15 01:50:50 +0300

    videoplayer-modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,11 @@
 import '../sass/main.scss';
-import fetchOneFilm from './fetchOneFilm';
+import {fetchOneFilm,fetchVideo} from './fetchOneFilm';
 import modalHbs from '../templates/modal.hbs'
+import iframe from '../templates/iframe.hbs'
 const filmCardEl = document.querySelector(".film-card__list")
 const backdrop = document.querySelector(".backdrop")
-import { chekFilmByIdWatched, addToLocalStorageWatchedFilm, removeWatchedFilmFromLocalStorage, addToLocalStorageQueueFilm, removeQueueFilmFromLocalStorage, chekFilmByIdQueue } from "./localStorage"
+import { chekFilmByIdWatched, addToLocalStorageWatchedFilm, removeWatchedFilmFromLocalStorage,
+   addToLocalStorageQueueFilm, removeQueueFilmFromLocalStorage, chekFilmByIdQueue } from "./localStorage"
 let obj
 let id
 
@@ -12,9 +14,8 @@ filmCardEl.addEventListener("click", openModal);
 
 function openModal(event) {
   event.preventDefault();
-  //console.log(event);
   const { target } = event;
-  // Чи попав ти в картку?
+
   const areYouMissCard = !target.closest(".film-card__link");
   if (areYouMissCard) {
     console.log("you miss");
@@ -29,7 +30,15 @@ function openModal(event) {
     obj = res,
     backdrop.style.backgroundImage = `linear-gradient(to right, rgba(47,48,58,0.4), rgba(47,48,58,0.4)),url(https://image.tmdb.org/t/p/original/${res.backdrop_path}`
     const markup = modalHbs(res)
-    backdrop.insertAdjacentHTML("beforeend", markup)
+    backdrop.insertAdjacentHTML("afterbegin", markup)
+    createIframeElment()
+    const imgEl = document.querySelector(".modal-movie__youtube")
+    fetchVideo(idFilm).then(res =>{
+      if (!res.results[0]) {
+        imgEl.classList.add('visually-hidden')
+      }
+     })
+    imgEl.addEventListener("click",renderPlayerVideo)
     checkFilmLocalWatched(idFilm)
     checkFilmLocalQueue(idFilm)
     toggleModal()
@@ -46,6 +55,7 @@ function closeBtnModal() {
 function closeEscModal(event) {
   if (event.code === "Escape") {
     toggleModal()
+    document.querySelector(".wrapp-iframe").innerHTML=""
   }
 };
 
@@ -65,7 +75,7 @@ function checkFilmLocalWatched(idFilm) {
   const watchedBtnEl = document.querySelector("[data-btn-watched]")
   watchedBtnEl.addEventListener("click", ToLocalStorageWatched)
   const flag = chekFilmByIdWatched(idFilm)
-  flag ? watchedBtnEl.textContent = 'ADDED TO WATCHED' : watchedBtnEl.textContent = ' ADD TO WATCHED'
+  flag ? watchedBtnEl.textContent = 'Remove from WATCHED' : watchedBtnEl.textContent = ' ADD TO WATCHED'
   flag ? watchedBtnEl.classList.add("active") : watchedBtnEl.classList.remove("active")
 }
 
@@ -86,7 +96,7 @@ function checkFilmLocalQueue(idFilm) {
   const queueBtnEl = document.querySelector("[data-btn-queue]")
   queueBtnEl.addEventListener("click", ToLocalStorageQueue)
   const flag = chekFilmByIdQueue(idFilm)
-  flag ? queueBtnEl.textContent = 'ADDED TO QUEUE' : queueBtnEl.textContent = ' ADD TO QUEUE'
+  flag ? queueBtnEl.textContent = 'Remove from queue' : queueBtnEl.textContent = ' ADD TO QUEUE'
   flag ? queueBtnEl.classList.add("active") : queueBtnEl.classList.remove("active")
 
 }
@@ -101,8 +111,35 @@ function ToLocalStorageQueue() {
   }
   checkFilmLocalQueue(id)
 }
+function createIframeElment(params) {
+  const divIframeEl = document.createElement("div")
+  divIframeEl.classList.add("wrapp-iframe")
+  divIframeEl.classList.add("visually-hidden")
+  backdrop.append( divIframeEl)
+}
 
 
+  function renderPlayerVideo(event) {
+    const click = event.target.classList.contains("modal-movie__youtube");
+    if (!click) {
+      return
+    }
+    const video = fetchVideo(id).then(res =>{showVideo(res.results[0])})
+    console.log(video);
+  }
 
+function showVideo(params) {
+  const iframeEL = document.querySelector(".wrapp-iframe")
+  iframeEL.innerHTML=""
+  const data = iframe(params)
+  iframeEL.insertAdjacentHTML("beforeend", data)
+  iframeEL.classList.remove("visually-hidden")
+  document.querySelector(".close-video").addEventListener("click",closeVideo) 
+  document.querySelector(".video-iframe")
+}
 
+function closeVideo(params) {
+  document.querySelector(".wrapp-iframe").innerHTML=""
+  document.querySelector(".wrapp-iframe").classList.add("visually-hidden")
+}
 
