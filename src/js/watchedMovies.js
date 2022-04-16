7cdbff58463c6851238b9f50d8a6a950affa7c7d
sashadyg <dyg.sasha12@gmail.com>
2022-04-16 15:32:15 +0300

    Update watchedMovies.js

--- src/js/watchedMovies.js
+++ src/js/watchedMovies.js
@@ -1,48 +1,37 @@
-import fetchCountries  from './fetchOneFilm';
-import cardHBS from '../templates/card.hbs';
 import { getWatchedFilmFromLocalStorage } from './localStorage'
+import { markupGalleryWithPagination } from './markupGallery';
+import { genres } from './genres';
+import { formattingData } from './formattingData';
 
-const listRef = document.querySelector('.film-card__list')
 const watchedButtonRef = document.querySelector('#header__btn--watched')
 const queueButtonRef = document.querySelector('#header__btn--queue')
+const pagginationButtonRef = document.querySelectorAll('.pagination__button__current')
+const modalRef = document.querySelector('.modal-movie')
+const moviePerPage = 20
 
-watchedButtonRef.addEventListener('click', markupWatchedMovies)
+if(watchedButtonRef != undefined) {
+    watchedButtonRef.addEventListener('click', markupWatchedMovies)
+}
 
-// Імітація роботи localStorage
+export function markupWatchedMovies() {
+    const watchedFilmsFromLocal = getWatchedFilmFromLocalStorage()
 
-// const ids = [27, 14, 15, 100, 28, 24, 5, 13, 64, 55, 76, 87, 98, 11, 19, 20]
-// localStorage.setItem('films-for-watched', JSON.stringify(ids))  
-
-// Малює галерею
-
-function markupWatchedMovies(e) {
-    e.preventDefault();
-    listRef.innerHTML = '';
-
-    // забираю id
-
-    const localId = getWatchedFilmFromLocalStorage()
-
-    // вішаю класи активної кнопки
+    // вішаю класи на кнопки watched, queue
 
     watchedButtonRef.classList.remove("transparent-btn")
     queueButtonRef.classList.add("transparent-btn")
 
-    // якщо немає фільмів
+    // беру активну кнопку пагінації і забираю з неї число, а потім за допомогаю слайсу отримаю потрібні 20 фільмів
 
-    if(localId === null) {
-        return alert(
-            "watch the movie and add to watched list"
-        )
-    }
-    
-    // добавляю фільми у розмітку
+    const pageNumber = pagginationButtonRef.innerHTML
+    const startSlice = (pageNumber - 1) * moviePerPage
+    const endSlice = startSlice + moviePerPage
 
-    localId.map(id => {
-        fetchCountries(id)
-        .then(movie => {
-            const markup = cardHBS(movie)
-            listRef.insertAdjacentHTML('beforeend', markup)
-        })
-    })
+    const slicedArrayOfMovies = watchedFilmsFromLocal.slice(startSlice, endSlice)
+
+    // відображаю ці 20 фільмів
+
+    const formattedData = formattingData(slicedArrayOfMovies, genres);
+    markupGalleryWithPagination(formattedData);
 }
+  
