079734dafb01520202f1dc2c0043c2a92ddded57
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-04-17 14:16:20 +0300

    paginationToTheKeywordSection

--- src/js/keyword-search.js
+++ src/js/keyword-search.js
@@ -1,10 +1,11 @@
-import { fetchFilmsByKeywords } from './fetchFilmsByKeywords.js';
+import  NewApiFilmsByKeywords  from './fetchFilmsByKeywords.js';
 import { markupGalleryWithPagination } from './markupGallery.js';
 import { genres } from './genres';
 import { formattingData } from './formattingData';
 import { showSpinner, hideSpinner } from './spinner';
+import {  renderPagination } from './paginationRenderer'
 
-
+const newApiFilmsByKeywords = new NewApiFilmsByKeywords();
 // Функція обробник очищення поля для повідомлень
 export function clearErrorField() {
     const textError = document.querySelector('#header__container-msg'); // поле для отображения текста ошибки
@@ -33,15 +34,17 @@ export async function entryKeyWords(input) {
         searchForm.reset();
         return;
     }
+    newApiFilmsByKeywords.query = keyWord;
     try {
-        const films = await fetchFilmsByKeywords(keyWord);
+        const films = await newApiFilmsByKeywords.fetchFilmsByKeywords();
         if (films.results.length === 0) {
             onFetchError();
             return;
         }
         const formattedData = formattingData(films.results, genres);
         markupGalleryWithPagination(formattedData); //передача результатов на отрисовку
-        
+        createPagination();
+
     }
     catch (error) {
         onFetchError();
@@ -54,4 +57,33 @@ function onFetchError() {  //ошибка и очистка формы
     textError.textContent = 'Search result not successful. Enter the correct movie name and try again';
     searchForm.reset();
 }
+function createPagination() { 
+    const settings =
+        {
+            page: newApiFilmsByKeywords.pageNum,
+            totalCountItem: 500,
+            itemPerPage: 20,
+            ancestorID: "film-list__section",
+            insertPlace: "beforeEnd",
+        }
+        renderPagination(settings);
+const paginationWrapper = document.querySelector('#pagination-button__list-container');
+paginationWrapper.addEventListener('click', paginationListenerKeyword);
+}
 
+async function paginationListenerKeyword(event) { 
+    try {
+        event.preventDefault();
+        const { target } = event;
+        const nextPage = target.getAttribute('data-pageNumber');
+        newApiFilmsByKeywords.pageNum = nextPage;
+        console.log(newApiFilmsByKeywords.page);
+        const films = await newApiFilmsByKeywords.fetchFilmsByKeywords();
+        const formattedData = formattingData(films.results, genres);
+        markupGalleryWithPagination(formattedData); 
+        createPagination();
+    }
+    catch(error) { 
+        onFetchError();
+    } 
+} 
