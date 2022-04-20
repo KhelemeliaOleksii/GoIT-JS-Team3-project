bbdd1cfc79431a244b5af8d282b3ce95db40872a
KhelemeliaOleksii <xvdm@ukr.net>
2022-04-20 18:31:23 +0300

    Update fetchNews.js

--- src/js/fetchNews.js
+++ src/js/fetchNews.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const BASE_URL = 'https://newsdata.io';
-const API_KEY = 'pub_6434779cb2ad2e394f72bbc3e1c3695b3c76';
+// const API_KEY = 'pub_6434779cb2ad2e394f72bbc3e1c3695b3c76';
+const API_KEY = 'pub_6625c592a39f3f800acedaad5a1df9757dd5';
 
 export async function fetchNews() { 
     const { data: response } = await axios.get(`${BASE_URL}/api/1/news?apikey=${API_KEY}&q=films&language=en`)
